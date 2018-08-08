ef59d041c634eb3554d94d983e459a346b1cd494
Vanessa Toro <vanetoro84@gmail.com>
2018-08-08 18:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getRepositories(){
   req.send()
 }
 
-function showRepositories(){
+function showRepositories(event,data){
   const repos = JSON.parse(this.responseText)
   const src = document.getElementById("repository-template").innerHTML
   const template = Handlebars.compile(src)
